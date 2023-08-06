62862996910e22c9411ddf705b77127c9d50cb8f
fxsla <fuhadulislam02@gmail.com>
2023-08-06 12:36:44 +0100

    Finish fundamentals 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if(num1 < 0 || num2 < 0 ){
+        return 'ERROR';
+    }
+
+
+    let largestNumber = Math.max(num1, num2);
+    let smallestNumber = Math.min(num1, num2);
+
+    let totalSum = smallestNumber
+
+    for(let i=totalSum+1; i<=largestNumber;i++){
+        totalSum += i
+    }
+
+    return totalSum;
 
 };
 
